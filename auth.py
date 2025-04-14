86a27678deecfa770178428ddc4213cc92c0659d
dhio-n <142285791+dhio-n@users.noreply.github.com>
2025-04-14 10:05:24 -0300

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,22 @@
+# auth.py
+import streamlit as st
+from database import autenticar_usuario
+
+def login_form():
+    st.title("🔐 Login Administrativo")
+
+    usuario = st.text_input("Usuário")
+    senha = st.text_input("Senha", type="password")
+    login_btn = st.button("Entrar")
+
+    if login_btn:
+        if autenticar_usuario(usuario, senha):
+            st.session_state.logged_in = True
+            st.session_state.usuario = usuario
+            st.rerun()
+        else:
+            st.error("Usuário ou senha inválidos, ou acesso não autorizado.")
+
+def check_login():
+    if "logged_in" not in st.session_state:
+        st.session_state.logged_in = False
